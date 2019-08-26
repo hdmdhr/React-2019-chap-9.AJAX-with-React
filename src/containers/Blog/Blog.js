65f38742dc6361b929fcfc55a0d66f5844eed03c
DongMing <hdmdhr@gmail.com>
2019-08-25 18:44:19 -0600

    9.190-192: Use axios send HTTP request to JSONPlaceholder, get back fake posts, feed components.

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios'
 
 import Post from '../../components/Post/Post';
 import FullPost from '../../components/FullPost/FullPost';
@@ -6,13 +7,34 @@ import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
 
 class Blog extends Component {
+    // State
+    state = {
+        posts: []
+    }
+
+    // Lifecycles
+    componentDidMount() {
+        axios.get('https://jsonplaceholder.typicode.com/posts')
+        .then(res => {
+            const recentPosts = res.data.slice(0, 4)
+            const updatedPosts = recentPosts.map(post => {
+                return {...post, author: 'Daniel'}
+            })
+            this.setState({posts: updatedPosts})
+        })
+    }
+
     render () {
+        const posts = this.state.posts.map(post => (
+            <Post key={post.id} 
+            title={post.title} 
+            author={post.author} />
+        ))
+
         return (
             <div>
                 <section className="Posts">
-                    <Post />
-                    <Post />
-                    <Post />
+                    {posts}
                 </section>
                 <section>
                     <FullPost />
