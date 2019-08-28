5c2462b5f838871db0558be8576437bd37e5dc92
DongMing <hdmdhr@gmail.com>
2019-08-27 22:30:00 -0600

    9.199-200: Use axios.interceptors to do req & res debug. | Disable interceptors cleanly.

--- src/index.js
+++ src/index.js
@@ -3,6 +3,31 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import axios from 'axios';
+
+const reqInterceptor = axios.interceptors.request.use(reqConfig => {
+    console.log("REQ CONFIG",reqConfig);
+    // can edit request before return (sending)
+    return reqConfig
+},
+error => {
+    console.log(error);  // request sending error
+    return Promise.reject(error)
+})
+
+
+const resInterceptor = axios.interceptors.response.use(res => {
+    console.log("RES", res);
+    return res
+},
+error => {
+    console.log(error);
+    return Promise.reject(error)
+})
+
+// disable interceptors
+axios.interceptors.request.eject(reqInterceptor)
+axios.interceptors.response.eject(resInterceptor)
 
 ReactDOM.render( <App />, document.getElementById( 'root' ) );
 registerServiceWorker();
