2f6b4a39ab5a8dc2fe55722aa3ddbfe1c65480f6
DongMing <hdmdhr@gmail.com>
2019-09-07 21:13:56 -0600

    11.220: Remove post from Blog.js, create a <Posts> component to only display on home route.

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,51 +1,11 @@
 import React, { Component } from 'react';
-// import axios from 'axios'
-import axiosInstance from '../../axios';
 
-import Post from '../../components/Post/Post';
-import FullPost from './FullPost/FullPost';
-import NewPost from './NewPost/NewPost';
 import './Blog.css';
+import Posts from './Posts/Posts';
 
 class Blog extends Component {
-    // State
-    state = {
-        posts: [],
-        selectedPostId: null,
-        error: false
-    }
-
-    // Lifecycles
-    componentDidMount() {
-        axiosInstance
-        .get('/posts')
-        .then(res => {
-            const recentPosts = res.data.slice(0, 4)
-            const updatedPosts = recentPosts.map(post => {
-                return {...post, author: 'Daniel'}
-            })
-            this.setState({posts: updatedPosts})
-        })
-        .catch(err => {
-            this.setState({error: true})         
-        })
-    }
-
-    // Callback Methods
-    postSelectedHandler = id => {
-        this.setState({selectedPostId: id})
-    }
 
     render () {
-        let posts = <h4 style={{textAlign: 'center', color: 'red'}}>Something went wrong and cannot get blogs.</h4>
-        if (!this.state.error) {
-            posts = this.state.posts.map(post => (
-                <Post key={post.id} 
-                title={post.title} 
-                author={post.author}
-                clicked={() => this.postSelectedHandler(post.id)} />
-            ))
-        }
 
         return (
             <div className='Blog'>
@@ -57,15 +17,7 @@ class Blog extends Component {
                         </ul>
                     </nav>
                 </header>
-                <section className="Posts">
-                    {posts}
-                </section>
-                <section>
-                    <FullPost id={this.state.selectedPostId} />
-                </section>
-                <section>
-                    <NewPost />
-                </section>
+                <Posts />
             </div>
         );
     }
---
+++ src/containers/Blog/Posts/Posts.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react'
+// import axios from 'axios'
+import axiosInstance from '../../../axios';
+import Post from '../../../components/Post/Post';
+
+import './Posts.css'
+
+class Posts extends Component {
+    // State
+    state = {
+        posts: []
+    }
+
+
+    // Lifecycles
+    componentDidMount() {
+        axiosInstance
+            .get('/posts')
+            .then(res => {
+                const recentPosts = res.data.slice(0, 4)
+                const updatedPosts = recentPosts.map(post => {
+                    return { ...post, author: 'Daniel' }
+                })
+                this.setState({ posts: updatedPosts })
+            })
+            .catch(err => {
+                console.log(err);
+            })
+    }
+
+    // Callback Methods
+    postSelectedHandler = id => {
+        this.setState({ selectedPostId: id })
+    }
+
+    render() {
+        let posts = <h4 style={{ textAlign: 'center', color: 'red' }}>Something went wrong and cannot get blogs.</h4>
+        if (!this.state.error) {
+            posts = this.state.posts.map(post => (
+                
+                <Post key={post.id}
+                    title={post.title}
+                    author={post.author}
+                    clicked={() => this.postSelectedHandler(post.id)} />
+            ))
+        }
+
+        return (
+            <section className="Posts">
+                {posts}
+            </section>
+        )
+    }
+}
+
+export default Posts
