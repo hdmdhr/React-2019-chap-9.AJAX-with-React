de47d5fee3a4f69dc61c4597dcc3f4081e540b89
DongMing <hdmdhr@gmail.com>
2021-01-16 14:01:24 -0700

    Use withRouter HOC to access routing props in lower level components

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom';
 
 import './Post.css';
 
@@ -12,4 +13,4 @@ const post = props => (
     </article>
 );
 
-export default post;
+export default withRouter(post);
