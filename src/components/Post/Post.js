fd1d1393335c91bfe1ff8b2b5506c77f9f99f513
DongMing <hdmdhr@gmail.com>
2021-01-16 14:02:39 -0700

    Revert "Use withRouter HOC to access routing props in lower level components"
    This reverts commit de47d5fee3a4f69dc61c4597dcc3f4081e540b89.

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { withRouter } from 'react-router-dom';
 
 import './Post.css';
 
@@ -13,4 +12,4 @@ const post = props => (
     </article>
 );
 
-export default withRouter(post);
+export default post;
