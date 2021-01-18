8a2a547da7797ab607e715e94ee9b3317187ad5c
DongMing <hdmdhr@gmail.com>
2021-01-17 22:30:26 -0700

    Programmatically navigate without <Link>, using props.history.push

--- src/containers/Blog/Posts/Posts.js
+++ src/containers/Blog/Posts/Posts.js
@@ -33,18 +33,20 @@ class Posts extends Component {
 
     // Callback Methods
     postSelectedHandler = id => {
-        this.setState({ selectedPostId: id })
+        this.props.history.push({ pathname: '/' + id });
+        // this.props.history.push('/' + id);
     }
 
     render() {
         let posts = <h4 style={{ textAlign: 'center', color: 'red' }}>Something went wrong and cannot get blogs.</h4>
         if (!this.state.error) {
             posts = this.state.posts.map(post => (
-                <Link to={'/' + post.id} key={post.id}>
-                    <Post title={post.title}
-                        author={post.author}
-                        clicked={() => this.postSelectedHandler(post.id)} />
-                </Link>
+                // <Link to={'/' + post.id} key={post.id}>
+                <Post key={post.id}
+                    title={post.title}
+                    author={post.author}
+                    clicked={() => this.postSelectedHandler(post.id)} />
+                // </Link>
             ))
         }
 
